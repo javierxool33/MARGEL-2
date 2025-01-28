e0aef432e4a2e74abb5e0bae6b4224bf49eacb3c
Francisco <franciscojavierxoolrosas@gmail.com>
2025-01-28 13:54:25 -0600

    cambios terminados

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('cuerpo-dev1').style.display = 'block';
+    }, 10000); // 10000 milisegundos = 10 segundos
+};
